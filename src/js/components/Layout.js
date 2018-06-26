efb709d2d5a3d6d8d518cc672099c9188e0bd1ed
Jackson <jackson.hardaker@gmail.com>
2018-06-26 16:59:59 -0400

    Add redux and file architecture

---
+++ src/js/components/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { connect } from "react-redux";
+
+/*@connect((store) => {
+    return {
+    };
+  });*/
+
+export default class Layout extends React.Component {
+    componentWillMount() {
+        // Do something when component is created
+    }
+
+    render() {
+
+        return <div>
+            <h1>Stuff</h1>
+        </div>
+    }
+}
---
+++ src/js/reducers/index.js
@@ -0,0 +1,5 @@
+//import { combineReducers } from "redux"
+
+export default function reducer(state = {}, action) {
+    return state;
+}
